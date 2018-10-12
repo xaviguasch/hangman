82ca89ef9f9207c4c0af6fa9acd9f9f2e96870c6
xaviguasch <xaviguasch@gmail.com>
2018-10-12 15:43:01 +0200

    playing with sync/async

--- app.js
+++ app.js
@@ -17,36 +17,40 @@ window.addEventListener('keypress', (e) => {
   guessesEl.textContent = game1.statusMessage
 })
 
+
+
+
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
+  }
+})
+
+
+
+
+
+
+
+
+
 // Making an HTTP request
-// const request = new XMLHttpRequest() 
-
-// request.addEventListener('readystatechange', (e) => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-    
-//     const data = JSON.parse(e.target.responseText)
-//     console.log(data);
-//   } else if (e.target.readyState === 4) {
-//     console.log('An error has taken place');
-    
-//   }
-// })
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-// request.send()
 
 
 /////////////
-const countryRequest = new XMLHttpRequest()
-const countryCode = 'ES'
+// const countryRequest = new XMLHttpRequest()
+// const countryCode = 'ES'
 
-countryRequest.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find(((e) => e.alpha2Code === countryCode))
-    console.log(country.name);
-  } else if (e.target.readyState === 4) {
-    console.log('An error has taken place');
-  }
-})
-countryRequest.open('GET', `http://restcountries.eu/rest/v2/all`)
-countryRequest.send()
+// countryRequest.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find(((e) => e.alpha2Code === countryCode))
+//     console.log(country.name);
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place');
+//   }
+// })
+// countryRequest.open('GET', `http://restcountries.eu/rest/v2/all`)
+// countryRequest.send()
---
+++ requests.js
@@ -0,0 +1,35 @@
+const getPuzzle = (callback) => {
+  const request = new XMLHttpRequest() 
+
+  request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      callback(undefined, data.puzzle)
+    } else if (e.target.readyState === 4) {
+      callback('An error has taken place', undefined)
+      
+    }
+  })
+
+  request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+  request.send()
+
+  
+}
+
+
+
+
+
+// const getPuzzleSync = () => {
+//   const request = new XMLHttpRequest() 
+//   request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3', false)
+//   request.send()
+
+//   if (request.readyState === 4 && request.status === 200) {
+//     const data = JSON.parse(request.responseText)
+//     return data.puzzle
+//   } else if (request.readyState === 4) {
+//     throw new Error('Things did not go well!!!!')
+//   }
+// }
