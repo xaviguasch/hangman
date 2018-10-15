8778ac737086d4299394a8a521949323e78b3a5e
xaviguasch <xaviguasch@gmail.com>
2018-10-15 14:08:42 +0200

    githubpages file path issues 7

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-  const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
   if (response.status === 200) {
     const data = await response.json()
@@ -26,7 +26,7 @@ const getPuzzle = async (wordCount) => {
 
 // adding async/await
 const getCountry = async (countryCode) => {
-  const response = await fetch(`https://restcountries.eu/rest/v2/all`)
+  const response = await fetch(`http://restcountries.eu/rest/v2/all`)
       if (response.status === 200) {      
         const data = await response.json()
         return data.find(((country) => country.alpha2Code === countryCode))
